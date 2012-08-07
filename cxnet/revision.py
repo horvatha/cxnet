6d0499c3d7a1cd37f850414dc379c47d1156e8e1
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2012-08-07 21:24:57 +0200

    revision.py More general get_revision function

--- cxnet/revision.py
+++ cxnet/revision.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from __future__ import print_function
 import os
 import cxnet
 import subprocess
@@ -9,9 +10,20 @@ def cxnet_path():
     file_ = cxnet.__file__
     return os.path.join(os.path.split(file_)[0], "..")
 
-def get_revision():
+def get_revision(path=None):
+    """Get the revision of the path.
+
+    It can be git or bazaar repository.
+    Parameter:
+        path: string or None
+            The path of the repository.  If path is None, it
+            returns the revision of the cxnet package.
+
+    """
+    if path is None:
+        path = cxnet_path()
     for getter in [get_git_revision, get_bzr_revision]:
-        revision = getter(cxnet_path())
+        revision = getter(path)
         if revision:
             return revision
     return ""
@@ -39,4 +51,4 @@ def get_bzr_revision(path):
     return revno
 
 if __name__ == '__main__':
-    print "Revison {0}".format(get_revision())
+    print("Revison {0}".format(get_revision()))
